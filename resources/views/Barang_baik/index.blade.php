92c6c0b762e1a9d3c0532d5f0118fa55b61df85f
unknown <mularahman57@gmail.com>
2024-01-27 12:29:42 +0800

    fix button position

--- resources/views/Barang_baik/index.blade.php
+++ resources/views/Barang_baik/index.blade.php
@@ -24,36 +24,41 @@
                         </div>
 
                         <div class="p-5 ">
-                            <div class="flex justify-between">
-                            <div class="flex ">
-                                <a href="/cetakPDF_baik"
-                                    class="block me-2 text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800 "
-                                    type="button">
-                                     <i class="fas fa-print"></i> Cetak PDF
-                                </a>
-                                <a href="/cetakEXCEL_baik"
-                                    class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 "
-                                    type="button">
-                                     <i class="fas fa-print"></i> Cetak EXCEL
-                                </a>
-</div>
+                            <div class="flex justify-between gap-2 flex-col md:flex-row">
+                                <div class="flex gap-2 flex-col md:flex-row">
+                                    <a href="/cetakPDF_baik"
+                                        class="block  text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800 "
+                                        type="button">
+                                        <i class="fas fa-print"></i> Cetak PDF
+                                    </a>
+                                    <a href="/cetakEXCEL_baik"
+                                        class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 "
+                                        type="button">
+                                        <i class="fas fa-print"></i> Cetak EXCEL
+                                    </a>
+                                </div>
 
 
-<form action="" method="GET">
+                                <form action="" method="GET">
 
 
-        <div class="relative w-full">
-            <input type="search" name="search" id="search-dropdown" class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" placeholder="Search..." required value="{{request('search')}}">
-            <button type="submit" class="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                </svg>
-                <span class="sr-only">Search</span>
-            </button>
-        </div>
+                                    <div class="relative w-full">
+                                        <input type="search" name="search" id="search-dropdown"
+                                            class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
+                                            placeholder="Search..." required value="{{ request('search') }}">
+                                        <button type="submit"
+                                            class="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                            <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                                fill="none" viewBox="0 0 20 20">
+                                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                                    stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                                            </svg>
+                                            <span class="sr-only">Search</span>
+                                        </button>
+                                    </div>
 
-</form>
-</div>
+                                </form>
+                            </div>
 
 
                             <div class="relative overflow-x-auto shadow-md sm:rounded-lg pt-5">
@@ -145,9 +150,9 @@
                                     </tbody>
                                 </table>
                             </div>
-<div class="mt-5 flex justify-end">
-    {{$data->links('layouts.partial.pagination')}}
-</div>
+                            <div class="mt-5 flex justify-end">
+                                {{ $data->links('layouts.partial.pagination') }}
+                            </div>
                         </div>
                     </div>
                     <!--/Graph Card-->
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -277,7 +277,7 @@
                                                 </td>
                                                 <td class="px-6 py-4 flex ">
                                                     <a href="/detail/{{$item->id}}"
-                                                        class="block me-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-1/3 py-2.5  text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"><i
+                                                        class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 me-2"><i
                                                             class="fas fa-fw fa-eye text-blue"></i></a>
 
                                                               <!-- Modal delete toggle -->
